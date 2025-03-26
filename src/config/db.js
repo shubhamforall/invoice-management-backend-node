03484d198d83b047ef77462deb5320c96659a73c
shubhampatil0514 <patilshubham0514@gmail.com>
2025-03-26 20:42:14 +0530

    Update sequelize file

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,27 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
   logging: false,
 });
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
+    console.log("Database connected successfully!");
   } catch (error) {
+    console.error("Database connection failed:", error);
     process.exit(1);
   }
 };
+
+connectDB();
 
 module.exports = { sequelize, connectDB };
